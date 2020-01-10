7cb3dc3dbaccbd9cfc4a078b7baaf686077fda23
payneqin <qinyuanpei@163.com>
2020-01-10 13:21:26 +0800

    work with vuejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,8 +5,8 @@
 </template>
 
 <script>
-import { Scene, LineLayer, PointLayer, Popup } from "@antv/l7";
-import { Mapbox } from "@antv/l7-maps";
+import { Scene, PointLayer, Popup } from "@antv/l7";
+import { GaodeMap } from "@antv/l7-maps";
 
 export default {
   data() {
@@ -15,112 +15,46 @@ export default {
   mounted() {
     const scene = new Scene({
       id: "map",
-      map: new Mapbox({
+      map: new GaodeMap({
         pitch: 0,
-        style: {
-          version: 8,
-          sprite: "https://lzxue.github.io/font-glyphs/sprite/sprite",
-          glyphs:
-            "https://gw.alipayobjects.com/os/antvdemo/assets/mapbox/glyphs/{fontstack}/{range}.pbf",
-          sources: {},
-          layers: [
-            {
-              id: "background",
-              type: "background",
-              paint: {
-                "background-color": "#2b2b3a"
-              }
-            }
-          ]
-        },
-        center: [3.438, 40.16797],
-        zoom: 4
+        style: "dark",
+        center: [112, 23.69],
+        zoom: 2.5
       })
     });
-    Promise.all([
-      fetch(
-        "https://gw.alipayobjects.com/os/antvdemo/assets/data/world.geo.json"
-      ).then(d => d.json()),
-      fetch(
-        "https://gw.alipayobjects.com/os/basement_prod/4472780b-fea1-4fc2-9e4b-3ca716933dc7.json"
-      ).then(d => d.text()),
-      fetch(
-        "https://gw.alipayobjects.com/os/basement_prod/a5ac7bce-181b-40d1-8a16-271356264ad8.json"
-      ).then(d => d.text())
-    ]).then(function onLoad([world, dot, flyline]) {
-      const dotData = eval(dot);
-      const flydata = eval(flyline).map(item => {
-        const latlng1 = item.from.split(",").map(e => {
-          return e * 1;
-        });
-        const latlng2 = item.to.split(",").map(e => {
-          return e * 1;
-        });
-        return { coord: [latlng1, latlng2] };
-      });
-      // const worldFill = new PolygonLayer()
-      //   .source(world)
-      //   .color('#98E3FA')
-      //   .shape('fill')
-      //   .style({
-      //     opacity: 1
-      //   });
 
-      const worldLine = new LineLayer()
-        .source(world)
-        .color("#41fc9d")
-        .size(0.5)
-        .style({
-          opacity: 0.4
+    fetch("https://gw.alipayobjects.com/os/basement_prod/9078fd36-ce8d-4ee2-91bc-605db8315fdf.csv")
+      .then(res => res.text())
+      .then(data => {
+        const pointLayer = new PointLayer({})
+          .source(data, {
+            parser: {
+              type: "csv",
+              x: "Longitude",
+              y: "Latitude"
+            }
+          })
+          .shape("circle")
+          .active(true)
+          .animate(true)
+          .size(56)
+          .color("#4cfd47")
+          .style({
+            opacity: 1
+          });
+
+        pointLayer.on("mousemove", e => {
+          const popup = new Popup({
+            offsets: [0, 0],
+            closeButton: false
+          })
+            .setLnglat(e.lngLat)
+            .setHTML(`<span>车次: ${e.feature.info}</span>`);
+          scene.addPopup(popup);
         });
-      const dotPoint = new PointLayer()
-        .source(dotData, {
-          parser: {
-            type: "json",
-            x: "lng",
-            y: "lat",
-            info: "info"
-          }
-        })
-        .shape("circle")
-        .color("#ffed11")
-        .animate(true)
-        .size(40)
-        .style({
-          opacity: 1.0
-        });
-      dotPoint.on("mousemove", e => {
-        const popup = new Popup({
-          offsets: [0, 0],
-          closeButton: false
-        })
-          .setLnglat(e.lngLat)
-          .setHTML(`<span>车次: ${e.feature.info}</span>`);
-        scene.addPopup(popup);
+        
+        scene.addLayer(pointLayer);
       });
-      const flyLine = new LineLayer()
-        .source(flydata, {
-          parser: {
-            type: "json",
-            coordinates: "coord"
-          }
-        })
-        .color("#ff6b34")
-        .shape("arc3d")
-        .size(2)
-        .animate({
-          interval: 2,
-          trailLength: 2,
-          duration: 1
-        })
-        .style({
-          opacity: 1
-        });
-      // scene.addLayer(worldFill);
-      scene.addLayer(worldLine);
-      scene.addLayer(dotPoint);
-      scene.addLayer(flyLine);
-    });
   }
 };
 </script>
